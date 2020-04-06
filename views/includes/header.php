9bd7f082420b9f901bb171209a2b948786e4a858
Martin Lejeune <mlej7498@gmail.com>
2020-04-06 18:03:09 +0200

    Adaptation de boostrap et mise en place du MVC

---
+++ views/includes/header.php
@@ -0,0 +1,35 @@
+	<header class="header_area sticky-header">
+		<div class="main_menu">
+			<nav class="navbar navbar-expand-lg navbar-light main_box">
+				<div class="container">
+					<!-- Brand and toggle get grouped for better mobile display -->
+					<a class="navbar-brand logo_h" href="index.html"><img src="public/img/logo.png" alt=""></a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<!-- Collect the nav links, forms, and other content for toggling -->
+					<div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+						<ul class="nav navbar-nav menu_nav ml-auto">
+							<li class="nav-item active"><a class="nav-link" href="<?=ROOT_PATH?>">Accueil</a></li>
+							<li class="nav-item"><a class="nav-link" href="<?=ROOT_PATH?>/views/shop.html">Shop</a></li>
+							<li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+							<li class="nav-item submenu dropdown">
+								<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+								 aria-expanded="false">Connexion</a>
+								<ul class="dropdown-menu">
+									<li class="nav-item"><a class="nav-link" href="login.html">Se connecter</a></li>
+									<li class="nav-item"><a class="nav-link" href="signup.html">S'enregistrer</a></li>
+								</ul>
+							</li>
+						</ul>
+						<ul class="nav navbar-nav navbar-right">
+							<li class="nav-item"><a href="#" class="cart"><span class="ti-bag"></span></a></li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</header>
--- views/welcome.php
+++ views/welcome.php
@@ -1,5 +1,81 @@
+<?php ob_start() ?>
+	<!-- start banner Area -->
+	<section class="banner-area">
+		<div class="container">
+			<div class="row fullscreen align-items-center justify-content-start">	
+						<!-- single-slide -->
+						<div class="row single-slide">
+							<div class="col-lg-5">
+								<div class="banner-content">
+									<h1>Whey New <br>Collection!</h1>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+										dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
+									<div class="add-bag d-flex align-items-center">
+										<a class="add-btn" href=""><span class="lnr lnr-cross"></span></a>
+										<span class="add-text text-uppercase">Ajouter au panier</span>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-7">
+								<div class="banner-img">
+									<img class="img-fluid" src="public/img/banner-img.png" alt="">
+								</div>
+							</div>
+						</div>			
+			</div>
+		</div>
+	</section>
+	<!-- End banner Area -->
+	<!-- start features Area -->
+	<section class="features-area section_gap">
+		<div class="container">
+			<div class="row features-inner">
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="public/img/f-icon1.png" alt="">
+						</div>
+						<h6>Livraison Gratuite</h6>
+						<p>Livraison gratuite sur toutes nos commandes</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="public/img/f-icon2.png" alt="">
+						</div>
+						<h6>Politique de Retour</h6>
+						<p>Article endommage? Nous vous le remboursons</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="public/img/f-icon3.png" alt="">
+						</div>
+						<h6>Support</h6>
+						<p>Notre support est joignable h24, 7j/7</p>
+					</div>
+				</div>
+				<!-- single features -->
+				<div class="col-lg-3 col-md-6 col-sm-6">
+					<div class="single-features">
+						<div class="f-icon">
+							<img src="public/img/f-icon4.png" alt="">
+						</div>
+						<h6>Paiement securise</h6>
+						<p>Nous garantissons des transactions securisees</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- end features Area -->
 <?php
-$title="Bienvenue";
-$content="Welcome";
+$title="Welcome";
+$content=ob_get_clean();
 include 'includes/template.php';
 ?>
